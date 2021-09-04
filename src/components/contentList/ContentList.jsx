890233a42058f6249aecfa0272da63137591358b
ROHIT SINGH <rohit.s@menerva.tech>
2021-09-04 22:10:42 +0530

    slider

--- src/components/contentList/ContentList.jsx
+++ src/components/contentList/ContentList.jsx
@@ -1,4 +1,4 @@
-import { ArrowLeft, ArrowRight } from "@material-ui/icons";
+import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from "@material-ui/icons";
 import { useState } from "react";
 import { useRef } from "react";
 import ContentItem from "./ContentItem";
@@ -6,6 +6,7 @@ import "./contentlist.scss";
 
 const ContentList = () => {
 
+  const [ slideNo, setSlideNo ] = useState(0);
   const [ transX, setTransX ] = useState(0);
   const contentListRef = useRef();
 
@@ -13,22 +14,28 @@ const ContentList = () => {
     console.log("hi")
     let x = 0;
     const item = contentListRef.current;
-    if(direction === "left"){
+    if(direction === "left" && slideNo > 0){
+      setSlideNo(slideNo - 1);
         setTransX(transX => (transX + 15.5));
         // setTransX(transX + 15.5);
         x = transX + 15.5;
-    } else if(direction === "right") {
+        console.log(transX, x, slideNo);
+    item.style = `transform: translateX(${x}rem)`;
+    } else if(direction === "right" && slideNo < 5) {
+      setSlideNo(slideNo + 1);
       setTransX(transX - 15.5);
       x = transX - 15.5;
-    }
-    console.log(transX, x);
+      console.log(transX, x, slideNo);
     item.style = `transform: translateX(${x}rem)`;
+    }
   }
 
   return (
     <div className="content-container">
       <h1>Continue Watching</h1>
-      <ArrowLeft onClick={() => handleShift("left")} className="big-arrow" />
+      <ArrowBackIosOutlined onClick={() => handleShift("left")} className="big-arrow"
+        style={{display : slideNo === 0 ? "none" : "block"}}
+      />
       <div ref={contentListRef} className="content-list">
         <ContentItem />
         <ContentItem />
@@ -41,7 +48,9 @@ const ContentList = () => {
         <ContentItem />
         <ContentItem />
       </div>
-      <ArrowRight onClick={() => handleShift("right")} className="big-arrow big-arrow-right" />
+      <ArrowForwardIosOutlined onClick={() => handleShift("right")} className="big-arrow big-arrow-right" 
+      style={{display : slideNo > 4 ? "none" : "block"}}
+      />
     </div>
   )
 }
