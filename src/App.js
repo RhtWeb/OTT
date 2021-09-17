b82bc51cac790c15112f27f098bca2a1316e7d5c
ROHIT SINGH <rohit.s@menerva.tech>
2021-09-17 13:54:40 +0530

    register and login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.scss';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
 import Home from './pages/home/Home';
 
 
@@ -6,7 +8,9 @@ function App() {
   return (
     <div className="App">
       {/* <h1>Legend of Legends | RHTWEB</h1> */}
-      <Home />
+      {/* <Home /> */}
+      <Register />
+      {/* <Login /> */}
     </div>
   );
 }
