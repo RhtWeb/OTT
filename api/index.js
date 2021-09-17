55070437f0c7a3e11a3a98ad1bb380826e48024e
Rohit Singh <rhtsing@gmail.com>
2021-09-17 15:50:08 +0530

    DB and ENV Setup

--- api/index.js
+++ api/index.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const donenv = require("dotenv");
 
 const app = express();
+donenv.config();
+
+mongoose.connect(process.env.DB_URI)
+  .then(() => console.log("DB connected !"))
+  .catch(err => console.log(err.message));
 
 app.listen(7000, () => {
   console.log("Backend server running !");
