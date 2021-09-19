282c045ce4f93f5990b33d64f07d0cc62514d8b6
Rohit Singh <rhtsing@gmail.com>
2021-09-19 16:55:27 +0530

    register & login api

--- api/index.js
+++ api/index.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const donenv = require("dotenv");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 donenv.config();
 
 mongoose.connect(process.env.DB_URI)
-  .then(() => console.log("DB connected !"))
+  .then(() => console.log("DB connected!"))
   .catch(err => console.log(err.message));
 
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
 app.listen(7000, () => {
-  console.log("Backend server running !");
+  console.log("Backend server running!");
 });
