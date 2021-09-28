069d3f719a5f6886b3c05a34539d04dea3d5173f
Rohit Singh <rhtsing@gmail.com>
2021-09-28 19:15:21 +0530

    movie list routes

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const donenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const movieRoute = require("./routes/movies");
+const listRoutes = require("./routes/lists");
 
 const app = express();
 donenv.config();
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 app.use("/api/movie", movieRoute);
+app.use("/api/list", listRoutes);
 
 app.listen(7000, () => {
   console.log("Backend server running!");
